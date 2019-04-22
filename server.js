7f620f0a9e42ae201871f9e50d3d44d97ae7b373
IsmaelFarah101 <ismaelomermohamed1@gmail.com>
2019-04-22 04:32:33 -0500

    Second Commit

--- server.js
+++ server.js
@@ -2,19 +2,40 @@ let express = require("express")
 let bodyParser = require("body-parser")
 let Sequelize = require('sequelize')
 let api_routes = require("./routes/api.js")
+let path = require('path')
 
-sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './db.sqlite3'
-})
+db_url = process.env.DATABASE_URL
 
-sequelize.authenticate()
-.then(() => console.log('connected to sqlite'))
-.catch(err => console.log('error connecting', err))
+let sequelize
+
+if(db_url){
+    sequelize = new Sequelize(db_url, {
+        dialect: 'postgres',
+    })
+    sequelize.authenticate()
+    .then(() => console.log('connect to Postgresql'))
+    .catch(err => console.log(err))
+}
+else{
+    sequelize = new Sequelize({
+        dialect: 'sqlite',
+        storage: './db.sqlite3'
+    })
+    
+    sequelize.authenticate()
+    .then(() => console.log('connected to sqlite'))
+    .catch(err => console.log('error connecting', err))
+
+}
+
+
 
 let student = require('./model/student')(sequelize, Sequelize)
 let app = express()
 app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, 'student-sign-in-client', 'dist')))
+
 app.use('/api', api_routes(student))
 app.use(function(req, res, next){
     res.status(404).send('Not Found')
